7b622824edeb454475b9d1d985bd70bb44beb38f
Prasoon Agrawal <a.prasoon23@gmail.com>
2022-11-10 21:27:31 +0530

    Navigation in Compose

---
+++ app/src/main/java/com/example/fnavigation/Screen3.kt
@@ -0,0 +1,40 @@
+package com.example.fnavigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun Screen3(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar { Text(text = "Screen 3") }
+        }
+
+    ) {
+        // Screen content
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+
+        ) {
+
+
+            Text(text = "Screen 3")
+            Button(onClick = { navController.navigate(Destination.ScreenFourth.route) }) {
+                Text(text = "Next")
+            }
+        }
+    }
+
+}
